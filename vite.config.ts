13ef4b959e34515fbcd56c3a47493b39f3199f49
itsmeharryjk~ <iamharryjk@gmail.com>
2025-05-01 09:29:47 +0530

    fix: use relative base for assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: '/thattil-heritage/',
+base: './',
+
   server: {
     host: "::",
     port: 8080,
